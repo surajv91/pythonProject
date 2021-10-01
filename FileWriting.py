3d5d7d33d0caeea2b6581b6a9599c6775d4f6673
Suraj Vyas <surajv91@gmail.com>
2021-10-02 02:10:20 +0530

    FileWriting.py modified

--- FileWriting.py
+++ FileWriting.py
@@ -1,23 +1,24 @@
 f = open('First.txt', 'r')
 # f.write('\nThis is the first line')
-# a=f.read(3)
+# a=f.read(3) #prints number of characters in the bracket from the file
 # print(a)
-#
-# a=f.read(3)
-# print(a)
+
 # for line in f:
 #     print(line)
-# b=f.readline()
+
+# b=f.readline() #prints single lines from the file i.e one at a time
 # print(b)
+
 # t=f.tell()
 # print(t)
-# f.seek(0)
+# f.seek(0) #seek will get you to the desired character
 # c=f.readline()
 # print(c)
 
-c=f.readlines()
-print(c)
-for line in c:
-    print(line,end="")
+# c=f.readlines() #Readlines gives you a list
+
+# print(c)
+# for line in c:
+#     print(line,end="")
 
 f.close()
